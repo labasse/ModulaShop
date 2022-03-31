d5da392d4bffacaedb9db49175e7e488844ae6c7
Labasse <labasse@gmail.com>
2022-03-31 10:18:57 +0200

    Add ShoppingCart class

--- Cart/Dtos/CartDto.cs
+++ Cart/Dtos/CartDto.cs
@@ -1,6 +1,13 @@
-﻿    namespace Cart.Dtos
+﻿using Cart.Models;
+
+namespace Cart.Dtos
 {
     public record CartDto(Guid Id, LineDto[] Lines, decimal TotalPrice)
     {
+        public static CartDto FromCart(ShoppingCart cart)
+        {
+            // TODO : Créer un CartDto à partir d'un cart
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Cart/Models/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿namespace Cart.Models
+{
+    public class ShoppingCart
+    {
+        public class Item
+        {
+            public Item(int id, string name, decimal price)
+            {
+                IdProduct = id;
+                Name = name;
+                Price = price;
+            }
+            public int IdProduct { get; init; }
+            public int Qty { get; set; } = 1;
+            public string Name { get; init; }
+            public decimal Price { get; init; }
+        }
+        private List<Item> _items = new ();
+
+        public Guid Id { get; } = Guid.NewGuid ();
+        public IEnumerable<Item> Lines => throw new NotImplementedException(); // TODO
+        public decimal TotalPrice => throw new NotImplementedException(); // TODO
+
+        public Item GetItem(int index)
+        {
+            // TODO
+            throw new NotImplementedException ();
+        }
+        public void AddItem()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+        public void RemoveItem(int index)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
